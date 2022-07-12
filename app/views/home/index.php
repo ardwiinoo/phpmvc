c9487be3a50c537a804d3a832756e2f1a40cd4da
arif <ardwino88@gmail.com>
2022-07-12 11:53:37 +0700

    Modify: mengganti tampilan

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <center>
-        <h1>Selamat datang !!!</h1>
-    </center>
-</body>
-</html>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Selamat Datang!</h1>
+    <p class="lead">Project sederhana untuk memahami model, view, dan controller.</p>
+  </div>
+</div>
