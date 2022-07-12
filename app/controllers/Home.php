68c26ed86fd7999a9cd91d053077cae98e18cb9a
arif <ardwino88@gmail.com>
2022-07-12 13:06:27 +0700

    Add: menambahkan data user model

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,9 +5,10 @@ class Home extends Controller
     public function index()
     {
         $data['title'] = 'Home';
+        $data['name'] = $this->model('User_model')->getUser();
 
         $this->view('templates/header', $data);
-        $this->view('home/index');
+        $this->view('home/index', $data);
         $this->view('templates/footer');
     }
 }
