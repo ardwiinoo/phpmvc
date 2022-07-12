cd19d790fa6622891e57016905ee2175766c1b72
arif <ardwino88@gmail.com>
2022-07-12 20:07:59 +0700

    Modify: menambah modal dan form untuk insert data mhs

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,5 +1,11 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="col-6">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+            Tambah data
+        </button>
+
+        <br><br>
         <h3>Daftar Mahasiswa</h3>
 
         <ul class="list-group">
@@ -11,5 +17,52 @@
             <?php endforeach; ?>
         </ul>
 
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah data mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL ?>/mahasiswa/tambah" method="POST">
+                    <div class="form-group">
+                        <label for="name">Nama</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="nim">Nim</label>
+                        <input type="number" class="form-control" id="nim" name="nim">
+                    </div>
+                    <div class="form-group">
+                        <label for="prodi">Prodi</label>
+                        <select class="form-control" id="prodi" name="prodi">
+                            <option value="Informatika">Informatika</option>
+                            <option value="Teknik Kimia">Teknik Kimia</option>
+                            <option value="Teknik Elektro">Teknik Elektro</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Teknologi Pangan">Teknologi Pangan</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Insert</button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
