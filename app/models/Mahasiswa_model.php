3b066abce220fbe2b3faf2491a91b0eb041110f2
arif <ardwino88@gmail.com>
2022-07-12 20:07:19 +0700

    Modify: menambah method tambahDataMahasiswa()

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -12,14 +12,28 @@ class Mahasiswa_model
 
     public function getAllMahasiswa()
     {
-        $this->db->query('SELECT id, name, nim, prodi, email FROM ' . $this->table);
+        $this->db->query("SELECT id, name, nim, prodi, email FROM " . $this->table);
         return $this->db->resultSet();
     }
 
     public function getMahasiswaById($id)
     {
-        $this->db->query('SELECT name, nim, prodi, email FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->query("SELECT name, nim, prodi, email FROM " . $this->table . " WHERE id=:id");
         $this->db->bind('id', $id);
         return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $this->db->query("INSERT INTO " . $this->table . " VALUES ('', :name, :nim, :prodi, :email)");
+
+        $this->db->bind('name', $data['name']);
+        $this->db->bind('nim', $data['nim']);
+        $this->db->bind('prodi', $data['prodi']);
+        $this->db->bind('email', $data['email']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
     }
 }
