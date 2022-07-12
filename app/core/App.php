ae74d49a8558bbdd862f7be61dd36571714dbc72
arif <ardwino88@gmail.com>
2022-07-12 09:39:45 +0700

    Modify: menambah fitur pada fungsi parseURL

--- app/core/App.php
+++ app/core/App.php
@@ -11,7 +11,10 @@ class App
     public function parseURL()
     {
         if (isset($_GET['url'])) {
-            $url = $_GET['url'];
+
+            $url = rtrim($_GET['url'], '/'); // remove '/' on the last
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url); // split url
             return $url;
         }
     }
