355f4961f76215fa31797152869c87f15580d179
arif <ardwino88@gmail.com>
2022-07-12 20:40:21 +0700

    Modify: mengubah layout dan menambah method flash()

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,22 +1,31 @@
 <div class="container mt-3">
-    <div class="col-6">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
-            Tambah data
-        </button>
 
-        <br><br>
-        <h3>Daftar Mahasiswa</h3>
+    <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+        </div>
+    </div>
 
-        <ul class="list-group">
-            <?php foreach ($data['mhs'] as $mhs) : ?>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <?= $mhs['name']; ?>
-                    <a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">detail</a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+    <div class="row">
+        <div class="col-lg-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                Tambah data
+            </button>
 
+            <br><br>
+            <h3>Daftar Mahasiswa</h3>
+
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <?= $mhs['name']; ?>
+                        <a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">detail</a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+
+        </div>
     </div>
 </div>
 
